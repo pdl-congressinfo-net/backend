a6bc635c29608f024b7e6ebc31c059bd332cdd26
Daniel Pfurner <dpfurner@gmail.com>
2025-12-04 12:05:30 +0000

    Create sftp integration

--- app/core/files.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-from app.core.config import settings
-
-
-def sftp_connect():
-    transport = paramiko.Transport((settings.SFTP_HOST, settings.SFTP_PORT))
-    transport.connect(username=settings.SFTP_USER, password=settings.SFTP_PASSWORD)
-    return paramiko.SFTPClient.from_transport(transport)
